bf645f433cd6066000a7e8216e5ae74074e55bd1
hiechy <cho88520@naver.com>
2018-06-08 18:09:41 +0900

    Free + MemberVO + LibraryVO

---
+++ project_library/src/main/java/multi/project/library/LibraryVO.java
@@ -0,0 +1,89 @@
+package multi.project.library;
+
+public class LibraryVO {
+	
+	
+	private String l_id;
+	private String l_name;
+	private String l_location;
+	private String l_holiday;
+	private String l_time;
+	private String l_phone;
+	private String l_website;
+	private double l_latitude;
+	private double l_longtitude;
+	
+	
+	public LibraryVO() {
+		super();
+	}
+	public LibraryVO(String l_id, String l_name, String l_location, String l_holiday, String l_time, String l_phone,
+			String l_website, double l_latitude, double l_longtitude) {
+		super();
+		this.l_id = l_id;
+		this.l_name = l_name;
+		this.l_location = l_location;
+		this.l_holiday = l_holiday;
+		this.l_time = l_time;
+		this.l_phone = l_phone;
+		this.l_website = l_website;
+		this.l_latitude = l_latitude;
+		this.l_longtitude = l_longtitude;
+	}
+	public String getL_id() {
+		return l_id;
+	}
+	public void setL_id(String l_id) {
+		this.l_id = l_id;
+	}
+	public String getL_name() {
+		return l_name;
+	}
+	public void setL_name(String l_name) {
+		this.l_name = l_name;
+	}
+	public String getL_location() {
+		return l_location;
+	}
+	public void setL_location(String l_location) {
+		this.l_location = l_location;
+	}
+	public String getL_holiday() {
+		return l_holiday;
+	}
+	public void setL_holiday(String l_holiday) {
+		this.l_holiday = l_holiday;
+	}
+	public String getL_time() {
+		return l_time;
+	}
+	public void setL_time(String l_time) {
+		this.l_time = l_time;
+	}
+	public String getL_phone() {
+		return l_phone;
+	}
+	public void setL_phone(String l_phone) {
+		this.l_phone = l_phone;
+	}
+	public String getL_website() {
+		return l_website;
+	}
+	public void setL_website(String l_website) {
+		this.l_website = l_website;
+	}
+	public double getL_latitude() {
+		return l_latitude;
+	}
+	public void setL_latitude(double l_latitude) {
+		this.l_latitude = l_latitude;
+	}
+	public double getL_longtitude() {
+		return l_longtitude;
+	}
+	public void setL_longtitude(double l_longtitude) {
+		this.l_longtitude = l_longtitude;
+	}
+	
+		
+}
---
+++ project_library/src/main/java/multi/project/library/MemberVO.java
@@ -0,0 +1,62 @@
+package multi.project.library;
+
+public class MemberVO {
+
+	private int l_id;
+	private String m_id;
+	private String m_pw;
+	private String m_phone;
+	private String m_name;
+	private String m_pic;
+	
+	public MemberVO() {
+		super();
+	}
+	public MemberVO(int l_id, String m_id, String m_pw, String m_phone, String m_name, String m_pic) {
+		super();
+		this.l_id = l_id;
+		this.m_id = m_id;
+		this.m_pw = m_pw;
+		this.m_phone = m_phone;
+		this.m_name = m_name;
+		this.m_pic = m_pic;
+	}
+	public int getL_id() {
+		return l_id;
+	}
+	public void setL_id(int l_id) {
+		this.l_id = l_id;
+	}
+	public String getM_id() {
+		return m_id;
+	}
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+	public String getM_pw() {
+		return m_pw;
+	}
+	public void setM_pw(String m_pw) {
+		this.m_pw = m_pw;
+	}
+	public String getM_phone() {
+		return m_phone;
+	}
+	public void setM_phone(String m_phone) {
+		this.m_phone = m_phone;
+	}
+	public String getM_name() {
+		return m_name;
+	}
+	public void setM_name(String m_name) {
+		this.m_name = m_name;
+	}
+	public String getM_pic() {
+		return m_pic;
+	}
+	public void setM_pic(String m_pic) {
+		this.m_pic = m_pic;
+	}
+	
+	
+}
